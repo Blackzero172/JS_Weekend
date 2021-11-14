a63162398f7aded888905f1c140d6b7316a52cb0
BlackZero172 <Supermeega@outlook.com>
2021-11-14 10:17:21 +0200

    changed solution

--- 5.3.js
+++ 5.3.js
@@ -1,10 +1,12 @@
 function toCamelCase(string) {
 	for (let i = 0; i < string.length; i++) {
-		if (string[i] === "-") {
+		let regEx = /[-_]/;
+		if (regEx.test(string[i])) {
 			string = string.replace(string[i + 1], string[i + 1].toUpperCase());
-			string = string.replace("-", "");
+			string = string.replace(regEx, "");
 		}
 	}
 	return string;
 }
 console.log(toCamelCase("the-stealth-warrior"));
+console.log(toCamelCase("the_stealth_warrior"));
